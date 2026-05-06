6fd8ab1b5c924ba9f04ca13215d5db5b68b4b8f7
bapatata688 <b0ldm6diego@gmail.com>
2026-05-05 19:59:02 -0600

    sistema evaluacion virtualizado con conda

--- reporte_1/encuestados.py
+++ reporte_1/encuestados.py
@@ -1,26 +1,37 @@
 import csv
 
-# lista de encuestados
+# programa que calcula el total de encuestados en un csv.
+# pre: encuestados siendo el primer valor del csv.
+# post: mostrar el total de encuestados
+
+# lista de encuestados en la encuesta
+# pre: la lista esta vacia
+# post: la lista contiene los id de los encuestados
 encuestados = []
 # contador de encuestados
+# pre: empieza por cero
+# post: contiene la id_encuestado
 encuestado = 0
-# abrir las encuestas
+# abrir el csv como encuestas
+# pre: el csv esta una carpeta arriba respecto al script actual
+# post: lectura del csv
 with open(
     "../encuesta_ingenieria_10000_respuestas.csv", "r", encoding="utf_8"
 ) as encuestas:
-    # asignar la variable encuesta leyendo las encuestas
+    # lectura de encuestas con la libreria csv
     encuesta = csv.reader(encuestas)
-    # salto de la primera linea
+    # salto de primera linea
     next(encuesta)
-    # recorrer las filas de el csv, asignando la primera columna a la variable id_encuestado
+    # recorrer la primera columna de las encuestas
+    # pre: las id de encuestados son la primera columna en las encuestas
+    # post: encuestados contiene los id de los encuestados
     for encuesta in encuesta:
         id_encuestado = encuesta[0]
-        # si es la primera columna de la encuesta, se agrega el id de encuestado a la lst
         if id_encuestado:
             encuestados.append(id_encuestado)
-            # aumentar el contador
             encuestado += 1
-# el total de encuestados sera igual a la longitud de la lista
+# longitud de la lista de encuestados
+# pre: encuestados contiene solamente id de los encuestados
+# post: longitud de encuestados
 total_encuestados = len(encuestados)
-# imprimir el total de encuestados
 print(f"Total de encuestados: {total_encuestados}")
